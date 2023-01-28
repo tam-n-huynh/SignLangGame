9397062f4f197a577a52f231b2f5b8b85b93ed06
Julie Maricic-Detweiler <80288445+JulieMaricicDetweiler@users.noreply.github.com>
2023-01-28 14:24:23 -0500

    Created basic heading

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,23 @@ import './Home.css'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    let navigate = useNavigate();
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="welcome-section">
+                <div className="welcome-heading">
+                    <h1>Welcome</h1>
+                    <h1>to</h1>
+                    <h1>Game Name</h1>
+                    <div className="line"/>
+                </div>
+                <button className="play-now-button" onClick={() => {navigate("/quiz") }}>Play Now</button>
+                <p className="learn-more">Learn More</p>
+            </div>
+            <div className="about-section">
+
+            </div>
+        </div>
     );
 }
 
