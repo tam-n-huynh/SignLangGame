4c30613678ff06a68eecaaca9cc23b2487203944
Julie Maricic-Detweiler <80288445+JulieMaricicDetweiler@users.noreply.github.com>
2023-01-28 16:28:55 -0500

    added scrolling and wrote about section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,16 @@
 import './Home.css'
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
 
 import sign_welcome from '../../assets/sign-welcome.png'
 
 const Home = () => {
     let navigate = useNavigate();
+    let ref = useRef(null);
+
+    const handleClick = () => {
+        ref.current?.scrollIntoView({behavior: 'smooth'});
+    }
     return (
         <div className="home">
 
@@ -16,15 +22,40 @@ const Home = () => {
                 </div>
                 <button className="play-now-button" onClick={() => {navigate("/quiz") }}>Play Now</button>
                 <p className="learn-more">Learn More</p>
-                <p className="arrow down"></p>
+                <p className="arrow down" onClick={handleClick}></p>
             </div>
 
-            <div className="about-section">
+            <div ref={ref} className="about-section">
                 <section className="about-container">
-                    <p className="about-heading">About</p>
-                    <div className="line"/>
+                    <div className="about-heading">
+                        <p>About</p>
+                        <div className="line"/>
+                    </div>
+                    <p>
+                        Hello! 
+                    </p>
+                    <p>   
+                        Sign language game is an interactive game 
+                        created for the purpose of teaching people sign language.
+                    </p>
+                    <p>
+                        Our game supports two modes: "Learning" and "Quiz"
+                    </p>
+                    <p>
+                        This project bodes the use of a sign language recognition 
+                        model trained using the TensorFlow software, as well as an
+                        interactive front end created with React.
+                    </p>
+                    <p>
+                        See the source code <a href="https://github.com/tam-n-huynh/SignLangGame" target="_blank" rel="noreferrer">HERE</a>
+                    </p>
                 </section>
             </div>
+
+            <div className="options-section">
+
+            </div>
+
         </div>
     );
 }
