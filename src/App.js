c83783043f39e8fedc4fe1658de0d10df3a3fe8f
Julie Maricic-Detweiler <80288445+JulieMaricicDetweiler@users.noreply.github.com>
2023-01-28 13:20:56 -0500

    Routing support added; Created templates for the 3 pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Home from "./Pages/Home/Home.js"
+import Learn from "./Pages/Learn/Learn.js"
+import Quiz from "./Pages/Quiz/Quiz.js"
 
 function App() {
   return (
-    <div className="App">
-      Sign Language Game
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/learn" element={ <Learn />}/>
+        <Route path="/quiz" element={ <Quiz /> } />
+      </Routes>
+    </Router>
   );
 }
 
